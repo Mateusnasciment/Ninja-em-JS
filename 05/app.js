16ebfedd6bb74ce8880a2d686af8949fb9c3ed04
Mateus nascimento <mateus.nasciment@hotmail.com>
2022-10-05 09:56:13 -0300

    finished: 04 05 06

---
+++ 05/app.js
@@ -0,0 +1,16 @@
+// // console.log('olá')
+// // const email = mateus.nasciment@hotmail.com
+
+// // console.log('todo dia eu falo {email}')
+
+// const firstName = 'mat'
+// const lastName = 'nasci'
+// console.log(fullName[0])
+// //////
+// console.log(fullName[3])
+// ///
+// console.log(fullName.toUpperCase)
+/// metodo ToupperCase
+const email = "mateus.anscimento@hotmail.com"
+ const emailSlice = email.slice(0, 15)
+ console.log(emailSlice)
---
+++ 06/app.js
@@ -0,0 +1,3 @@
+const email = "mateus.anscimento@hotmail.com"
+ const emailSlice = email.slice(0, 15)
+ console.log(emailSlice)
